023370c522fa0b6eb67112c11c661b0de66e6162
Ivan Roa <31189913+jeisonroa1@users.noreply.github.com>
2018-05-14 13:46:00 -0500

    Update Escalon.py

--- Escalon.py
+++ Escalon.py
@@ -1,8 +1,6 @@
 ###################################################################
-#Fisica de Semiconductores - Universidad Distrital Francisco Jose de Caldas
-#Jeison Ivan Roa Mora - 20131005056
-#Gustavo Rivas Gutierrez - 20132005036 
-
+# Escalon
+#Jeison Ivan Roa Mora 
 ###################################################################
 # Importar librerias
 import numpy as np                       
